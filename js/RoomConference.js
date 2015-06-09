517463cacbf96298984e9b3fb6dca2864d04f801
alexbij <alex.bijsterveld@luminis.eu>
2015-06-09 20:56:41 +0200

    Removebug in Monutakamer gefixt

--- js/RoomConference.js
+++ js/RoomConference.js
@@ -12,47 +12,47 @@ RoomConference.prototype.constructor = RoomConference;
 		this.textLights = new TextLights(scene, {});		
 		
 		THREE.ImageUtils.loadTexture('images/monuta/monuta1.png', undefined, function(texture){
-			room.whiteboard = new DetailImage(scene, texture, {
+			room.drupal = new DetailImage(scene, texture, {
 				scale: 60,
 				degree: 120,
 				verticalDegree: -10,
 				radius: 10 * SCALE,
 			});
-            room.whiteboard.mesh.rotation.z = 0.15;
-            room.whiteboard.mesh.rotation.y = -0.5;
+            room.drupal.mesh.rotation.z = 0.15;
+            room.drupal.mesh.rotation.y = -0.5;
 		});
 
         THREE.ImageUtils.loadTexture('images/monuta/monuta2.png', undefined, function(texture){
-            room.whiteboard = new DetailImage(scene, texture, {
+            room.mvm = new DetailImage(scene, texture, {
                 scale: 67,
                 degree: 180,
                 verticalDegree: -44,
                 radius: 14 * SCALE
             });
-            room.whiteboard.mesh.rotation.z = -0.12;
-            room.whiteboard.mesh.rotation.x = -1;
+            room.mvm.mesh.rotation.z = -0.12;
+            room.mvm.mesh.rotation.x = -1;
         });
         
         THREE.ImageUtils.loadTexture('images/monuta/uitvaartkostenmeter.jpg', undefined, function(texture){
-            room.whiteboard = new DetailImage(scene, texture, {
+            room.uvkm = new DetailImage(scene, texture, {
                 scale: 60,
                 degree: 246,
                 verticalDegree: -4,
                 radius: 10 * SCALE
             });
-            room.whiteboard.mesh.rotation.z = -0.20;
-            room.whiteboard.mesh.rotation.x = 0;
+            room.uvkm.mesh.rotation.z = -0.20;
+            room.uvkm.mesh.rotation.x = 0;
         });
 
         THREE.ImageUtils.loadTexture('images/monuta/monuta_logo_rood.png', undefined, function(texture){
-            room.whiteboard = new DetailImage(scene, texture, {
+            room.logo = new DetailImage(scene, texture, {
                 scale: 5,
                 degree: 180,
                 verticalDegree: -10,
                 radius: 10 * SCALE
             });
             //room.whiteboard.mesh.position.y += 40;
-            room.whiteboard.mesh.rotation.z = -0.04;
+            room.logo.mesh.rotation.z = -0.04;
 //            room.whiteboard.mesh.rotation.y = -0.5;
 //            room.whiteboard.mesh.rotation.x = -1;
         });
@@ -71,28 +71,21 @@ RoomConference.prototype.constructor = RoomConference;
 		
 		this.remove = function(){
 			this.photoSphere.remove();
-
-			this.textLights.remove();
-			
+			this.textLights.remove();			
 			this.navigationArrowToArrival.remove();
 			
-			if (room.vandijk1 != undefined){
-				room.vandijk1.remove();
+			if (room.drupal != undefined){
+				room.drupal.remove();
 			}
-			if (room.vandijk2 != undefined){
-				room.vandijk2.remove();
+			if (room.mvm != undefined){
+				room.mvm.remove();
 			}
-			if (room.vandijk3 != undefined){
-				room.vandijk3.remove();
+			if (room.uvkm != undefined){
+				room.uvkm.remove();
 			}
-			if (room.whiteboard != undefined){
-				room.whiteboard.remove();
-			}
-			
-			// part
-			if (room.part2 != undefined){
-				room.part2.remove();
-			}
+			if (room.logo != undefined){
+				room.logo.remove();
+			}						
 			
 			// remove self
 			scene.removeRoom(this);
